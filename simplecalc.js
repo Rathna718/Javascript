5cefad1682a0913e4a694b62c7979715680fe797
Rathna718 <rathna0718@gmail.com>
2025-07-15 07:48:40 -0700

    Add files via upload

---
+++ simplecalc.js
@@ -0,0 +1,13 @@
+a=50;
+b=10;
+sum=a+b;
+sub=a-b;
+multi=a*b;
+div=a%b;
+console.log("Addition: "+sum);
+console.log("Subtraction: "+sub);
+console.log("Multiplicaion: "+multi);
+console.log("Division: "+div);
+
+
+
