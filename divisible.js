bf60ff2ec9e91b0d6232e38662e3f3ffbfe5b5a0
Rathna718 <rathna0718@gmail.com>
2025-07-15 07:47:13 -0700

    Add files via upload

---
+++ divisible.js
@@ -0,0 +1,10 @@
+let num = 15;
+if (num % 3 === 0 && num % 5 === 0)
+{
+    console.log(num + " is divisible by both 3 and 5.");
+}
+else
+{
+    console.log(num + " is not divisible by both 3 and 5.");
+}
+
