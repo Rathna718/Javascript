e52fb5a8ac562e67f0ce30ea04042b2f6b1a18bf
Rathna718 <rathna0718@gmail.com>
2025-07-16 08:30:06 -0700

    Add files via upload

---
+++ revloop.js
@@ -0,0 +1,14 @@
+for(i = 10; i>=1 ; i--) {
+console.log("for loop " ,i);
+}
+let j = 10;
+while (j >= 1) {
+    console.log("while is" ,j);
+    j--;
+}
+let k = 10;
+do {
+    console.log("do while ",k);
+    k--;
+}
+while (k >= 1);
