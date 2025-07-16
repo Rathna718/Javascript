3215f64c2c34189e70205bf7e7300b7ca6d5e730
Rathna718 <rathna0718@gmail.com>
2025-07-16 08:29:20 -0700

    Add files via upload

---
+++ loop.js
@@ -0,0 +1,14 @@
+for(i = 1; i<=10; i++) {
+console.log("for loop " ,i);
+}
+let j = 1;
+while (j <= 10) {
+    console.log("while is" ,j);
+    j++;
+}
+let k = 1;
+do {
+    console.log("do while ",i);
+    k++;
+}
+while (i <= 10);
